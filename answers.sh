4773444340b5bc7f65b4d2fa47bc8ff204837e37
wissal al haj <wissalalhj@gmail.com>
2021-12-13 14:01:06 +0200

    basic queries

---
+++ answers.sh
@@ -0,0 +1,20 @@
+//Basic Queries
+//1
+SELECT Name FROM students;
+//2
+SELECT * FROM students where Age > 30;
+//3
+SELECT Name FROM students where Gender="F" and Age = 30;
+//4
+SELECT Points FROM students where Name ="Alex";
+//5
+INSERT INTO Students (Name,Age,Gender,Points)VALUES('wissal','21','F','150');
+//6
+UPDATE Students
+SET Points=Points+1
+WHERE Name='Basma';
+//7
+UPDATE Students
+SET Points=Points-1
+WHERE Name='Alex';
+
