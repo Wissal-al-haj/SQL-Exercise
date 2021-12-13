10ac65cc00868d69fd3536d678eef054f0cd419f
wissal al haj <wissalalhj@gmail.com>
2021-12-13 15:10:42 +0200

    basic queries

--- answers.sh
+++ answers.sh
@@ -1,20 +1,20 @@
 //Basic Queries
 //1
-SELECT Name FROM students;
+"SELECT Name FROM students;"
 //2
-SELECT * FROM students where Age > 30;
+"SELECT * FROM students where Age > 30;"
 //3
-SELECT Name FROM students where Gender="F" and Age = 30;
+"SELECT Name FROM students where Gender="F" and Age = 30;"
 //4
-SELECT Points FROM students where Name ="Alex";
+"SELECT Points FROM students where Name ="Alex";"
 //5
-INSERT INTO Students (Name,Age,Gender,Points)VALUES('wissal','21','F','150');
+"INSERT INTO Students (Name,Age,Gender,Points)VALUES('wissal','21','F','150');"
 //6
-UPDATE Students
+"UPDATE Students
 SET Points=Points+1
-WHERE Name='Basma';
+WHERE Name='Basma';"
 //7
-UPDATE Students
+"UPDATE Students
 SET Points=Points-1
-WHERE Name='Alex';
+WHERE Name='Alex';"
 
